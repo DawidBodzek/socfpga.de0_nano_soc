073c225e0a8a38bfd6a8bd49d5d13b24306e078b
DawidBodzek <bodzekdawid@gmail.com>
2026-07-17 18:10:46 +0200

    Update ltc2308 driver

--- sw/meta-asicsagh/recipes-kernel/ltc2308/files/ltc2308.c
+++ sw/meta-asicsagh/recipes-kernel/ltc2308/files/ltc2308.c
@@ -13,127 +13,131 @@
 #include <linux/wait.h>
 #include <linux/platform_device.h>
 
-struct test {
+struct ltc2308 {
     struct platform_device *pdev;
     struct miscdevice mdev;
+    
     void __iomem *addr;
-
     wait_queue_head_t wait;
     atomic_t ready;
 };
 
-static irqreturn_t test_handler(int irq, void *dev_id)
+static irqreturn_t ltc2308_irq_handler(int irq, void *dev_id)
 {
-    struct test *test = (struct test *)dev_id;
+    struct ltc2308 *ltc2308 = (struct ltc2308 *)dev_id;
 
-    atomic_set(&test->ready, 1);
-    wake_up_interruptible(&test->wait);
+    atomic_set(&ltc2308->ready, 1);
+    wake_up_interruptible(&ltc2308->wait);
 
     pr_info("FPGA IRQ\n");
     return IRQ_HANDLED;
 }
 
-static ssize_t test_read(struct file *filp, char __user *buf,
+static ssize_t ltc2308_read(struct file *filp, char __user *buf,
     size_t count, loff_t *f_pos)
 {
-    struct test *test = container_of(filp->private_data, struct test, mdev);
+    struct ltc2308 *ltc2308 = container_of(filp->private_data, struct ltc2308, mdev);
     
-    atomic_set(&test->ready, 0);
-    iowrite32(1, test->addr);
+    atomic_set(&ltc2308->ready, 0);
+    iowrite32(1, ltc2308->addr);
 
-    if (wait_event_interruptible(test->wait, atomic_read(&test->ready))) {
-	return -ERESTARTSYS;
+    if (wait_event_interruptible(ltc2308->wait, atomic_read(&ltc2308->ready))) {
+	    return -ERESTARTSYS;
     }
 
-    pr_info("Ready asserted\n");
     return 0;
 }
 
-static const struct file_operations test_fops = {
+static const struct file_operations ltc2308_fops = {
     .owner = THIS_MODULE,
-    .read = test_read
+    .read = ltc2308_read
 };
 
-static int test_probe(struct platform_device *pdev)
+static int ltc2308_probe(struct platform_device *pdev)
 {
     int err;
     int irq;
-    struct test *test;
+    struct ltc2308 *ltc2308;
     struct resource *res;
 
-    test = devm_kzalloc(&pdev->dev, sizeof(struct test), GFP_KERNEL);
-    if (!test) {
-        dev_err(&pdev->dev, "failed to allocate struct test\n");
+    ltc2308 = devm_kzalloc(&pdev->dev, sizeof(struct ltc2308), GFP_KERNEL);
+    if (!ltc2308) {
+        dev_err(&pdev->dev, "failed to allocate struct ltc2308\n");
         err = -ENOMEM;
         return err;
     }
 
-    test->pdev = pdev;
-    platform_set_drvdata(pdev, test);
+    ltc2308->pdev = pdev;
+    platform_set_drvdata(pdev, ltc2308);
 
     res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
     if (!res) {
-	dev_err(&pdev->dev, "failed to get resource\n");
-	err = -EINVAL;
-	return err;
+	    dev_err(&pdev->dev, "failed to get resource\n");
+	    err = -EINVAL;
+	    return err;
     }
 
-    test->addr = devm_ioremap_resource(&pdev->dev, res);
+    ltc2308->addr = devm_ioremap_resource(&pdev->dev, res);
 
-    test->mdev.minor = MISC_DYNAMIC_MINOR;
-    test->mdev.name = "test";
-    test->mdev.fops = &test_fops;
+    init_waitqueue_head(&ltc2308->wait);
+    atomic_set(&ltc2308->ready, 0);
 
-    err = misc_register(&test->mdev);
+    irq = platform_get_irq(pdev, 0);
+    if (irq < 0) {
+        dev_err(&pdev->dev, "failed to get irq\n");
+        err = irq;
+        return err;
+    }
+
+    err = devm_request_irq(&pdev->dev, irq, ltc2308_irq_handler, 0, "ltc2308", ltc2308);
+    if (err) {
+        dev_err(&pdev->dev, "failed to request irq\n");
+        return err;
+    }
+
+    ltc2308->mdev.minor = MISC_DYNAMIC_MINOR;
+    ltc2308->mdev.name = "ltc2308";
+    ltc2308->mdev.fops = &ltc2308_fops;
+
+    err = misc_register(&ltc2308->mdev);
     if (err) {
         dev_err(&pdev->dev, "failed to register miscdevice\n");
         return err;
     }
 
-    irq = platform_get_irq(pdev, 0);
-
-    err = devm_request_irq(&pdev->dev, irq, test_handler, 0, "test_irq", test);
-    if (err) {
-	dev_err(&pdev->dev, "failed to request irq\n");
-	return err;
-    }
-
-    init_waitqueue_head(&test->wait);
-    atomic_set(&test->ready, 0);
-
     dev_info(&pdev->dev, "probed\n");
     return 0;
 }
 
-static int test_remove(struct platform_device *pdev)
+static int ltc2308_remove(struct platform_device *pdev)
 {
-    struct test *test = platform_get_drvdata(pdev);
+    struct ltc2308 *ltc2308 = platform_get_drvdata(pdev);
 
-    misc_deregister(&test->mdev);
+    misc_deregister(&ltc2308->mdev);
     dev_info(&pdev->dev, "removed\n");
     return 0;
 }
 
-static const struct of_device_id test_ids[] = {
-    { .compatible = "my_test"},
+static const struct of_device_id ltc2308_ids[] = {
+    { .compatible = "ltc2308"},
     { }
 };
 
-MODULE_DEVICE_TABLE(of, test_ids);
+MODULE_DEVICE_TABLE(of, ltc2308_ids);
 
-static struct platform_driver test_driver = {
-    .probe = test_probe,
-    .remove = test_remove,
+static struct platform_driver ltc2308_driver = {
+    .probe = ltc2308_probe,
+    .remove = ltc2308_remove,
     .driver = {
-        .name = "test_driver",
+        .name = "ltc2308_driver",
         .owner = THIS_MODULE,
-        .of_match_table = test_ids
+        .of_match_table = ltc2308_ids
     }
 };
 
-module_platform_driver(test_driver);
+module_platform_driver(ltc2308_driver);
 
-MODULE_DESCRIPTION("simple irq driver for testing");
+MODULE_DESCRIPTION("ltc2308 driver");
 MODULE_AUTHOR("Dawid Bodzek <dbodzek@student.agh.edu.pl>");
 MODULE_VERSION("1.0");
 MODULE_LICENSE("GPL v2");
